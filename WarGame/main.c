9482df113159a6e8999ee7c958a9b7ef30a47988
David Gallagher <jibisjib@gmail.com>
2017-12-19 21:41:56 +0000

    Not Working - testing..

--- WarGame/main.c
+++ WarGame/main.c
@@ -14,3 +14,68 @@
 	*If all cards played are duplicates, points roll over to the next round.
 	*If this occurs on the final round, the sum of points are lost from the game.
 */
+#include<stdio.h>
+
+//struct player {
+//	int playerNum;
+//	int hand[13];
+//	int score;
+//	char cardPlayed;
+//};
+typedef struct {
+		int playerNum;
+		int hand[13];
+		int score;
+		char cardPlayed;
+}player;
+
+create_player(int playerNum, int hand[13], int score, char cardPlayed);
+//void playerStart(struct player aPlayer);
+//struct player aPlayer;
+
+void main() {
+	//Data Members
+	int playerHand[13];
+	int numPlayers;
+	//counters
+	int i;
+
+	//Start Game
+	printf("Please enter number of players between 2 & 13: \n");
+	scanf("%d", &numPlayers);
+	printf("Beginning game for %d players.\n", numPlayers);
+	for (i = 0; i < numPlayers; i++) {
+		create_player(numPlayers,playerHand[13],0,'A');
+		for (i = 0; i < (sizeof(playerHand) / sizeof(playerHand[0])); i++) {
+			playerHand[i] = i + 1;
+	}
+	
+
+}// End Main
+
+
+struct player* create_player(int playerNum,int hand[13],int score,char cardPlayed)
+{
+	struct player* newPlayer = (struct player*)malloc(sizeof(struct player));
+	if (newPlayer)
+	{
+		newPlayer->playerNum = playerNum;
+		newPlayer->hand[13] = hand[13];
+		newPlayer->score = score;
+		newPlayer->cardPlayed = cardPlayed;
+	}
+
+	return newPlayer;
+}
+
+//struct playerStart(struct player p) {
+//	int i;
+//
+//	//create player hand
+//	for (i = 0; i < (sizeof(p.hand) / sizeof(p.hand[0])); i++) {
+//		p.hand[i] = i + 1;
+//		printf("%d", p.hand[i]);
+//		printf("\n");
+//	}
+//	return p;
+//}
