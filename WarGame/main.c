ba538ef168041857270c5dd98fb61146b8aa11f1
David Gallagher <jibisjib@gmail.com>
2017-12-19 18:29:35 +0000

    Empty Project for War Game

---
+++ WarGame/main.c
@@ -0,0 +1,16 @@
+/*	Author - David Gallagher - 
+	War - Card Game - Overview
+	Num Players: 2 - 10
+	Rounds: 13
+	Rules:
+	Each Player gets 13 Cards, one of each suit from 2 - Ace
+	Card Value: 2 - 14.
+	----
+	Each player plays a card per round - card expires once played.
+	The player who plays the highest value card wins the round.
+	The winner of the round gains points equal to the sum value of all cards played that round.
+	If more than one of any card is played, those players cancel each other out, 
+	with the highest Unique card winning the round.
+	*If all cards played are duplicates, points roll over to the next round.
+	*If this occurs on the final round, the sum of points are lost from the game.
+*/
